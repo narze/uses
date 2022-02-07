649c14e50ed7bd72b48c51f813712cc37e4a6abf
Manassarn Manoonchai <manassarn@gmail.com>
2022-02-07 22:04:48 +0700

    Add Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :items
+
+    root to: 'items#index'
+  end
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
